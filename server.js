e8bcb768f4623a16bbfdd8692d5fb2b840989052
Kevin <kevinjamesbiju2000@gmail.com>
2024-12-13 13:06:45 +0530

    added patch in server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: "https://skyscroll-frontend.onrender.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
